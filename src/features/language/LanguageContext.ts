132e6c0074db1d234ee0b2119a72815c000d7093
hyeonQyu <dhk0561@naver.com>
2025-12-15 00:07:24 +0900

    [DAN-54] feat: 모국어 및 학습 언어 Provider

---
+++ src/features/language/LanguageContext.ts
@@ -0,0 +1,42 @@
+'use client';
+
+import { SourceLanguage, TargetLanguage } from '@/features/language/language.types';
+import { createContext, useContext } from 'react';
+
+export const LanguageContext = createContext<
+  | {
+      sourceLanguage: SourceLanguage;
+      targetLanguage: TargetLanguage;
+      setSourceLanguage: (sourceLanguage: SourceLanguage) => void;
+      setTargetLanguage: (targetLanguage: TargetLanguage) => void;
+    }
+  | undefined
+>(undefined);
+
+export const useLanguageContext = () => {
+  const context = useContext(LanguageContext);
+  if (!context) {
+    throw new Error('LanguageContext를 사용하기 전에 LanguageProvider로 감싸주세요.');
+  }
+  return context;
+};
+
+export const useSourceLanguage = () => {
+  const { sourceLanguage } = useLanguageContext();
+  return sourceLanguage;
+};
+
+export const useTargetLanguage = () => {
+  const { targetLanguage } = useLanguageContext();
+  return targetLanguage;
+};
+
+export const useSetSourceLanguage = () => {
+  const { setSourceLanguage } = useLanguageContext();
+  return setSourceLanguage;
+};
+
+export const useSetTargetLanguage = () => {
+  const { setTargetLanguage } = useLanguageContext();
+  return setTargetLanguage;
+};
