aa6fb27cf89494e673b9c9e8209497ed7a3fadc6
hyeonQyu <dhk0561@naver.com>
2025-12-31 15:47:30 +0900

    [DAN-72] feat: 단어 상세 페이지 뒤로 가기 버튼 추가

--- src/app/explore/search/detail/page.tsx
+++ src/app/explore/search/detail/page.tsx
@@ -1,15 +1,12 @@
 'use client';
 
-import { PageViewContainer } from '@/components/PageViewContainer';
 import { SlideInContainer } from '@/components/SlideInContainer';
 import { ExploreDictionaryEntryDetailView } from '@/features/explore/components/ExploreDictionaryEntryDetailView';
 
 function ExploreDictionaryEntryDetailPage() {
   return (
     <SlideInContainer>
-      <PageViewContainer>
-        <ExploreDictionaryEntryDetailView />
-      </PageViewContainer>
+      <ExploreDictionaryEntryDetailView />
     </SlideInContainer>
   );
 }
--- src/features/explore/components/ExploreDictionaryEntryDetailView/ExploreDictionaryEntryDetailView.tsx
+++ src/features/explore/components/ExploreDictionaryEntryDetailView/ExploreDictionaryEntryDetailView.tsx
@@ -1,8 +1,15 @@
+import { PageViewContainer } from '@/components/PageViewContainer';
+import { SlideInContainer } from '@/components/SlideInContainer';
 import { DictionaryEntryByLanguage } from '@/features/dictionary';
 import { DictionaryJAEntryDetail } from '@/features/dictionary/components/DictionaryEntryDetail';
-import { useTypedSearchParams } from '@/routes';
+import { useTypedRouter, useTypedSearchParams } from '@/routes';
+import { Z_INDEX } from '@/styles/zIndex.constants';
+import { ArrowBack } from '@mui/icons-material';
+import { Box, IconButton, useTheme } from '@mui/material';
 
 function ExploreDictionaryEntryDetailView() {
+  const { palette, spacing } = useTheme();
+
   const searchParams = useTypedSearchParams('/explore/search/detail');
   const { language, ...entry } = searchParams;
 
@@ -11,6 +18,10 @@ function ExploreDictionaryEntryDetailView() {
     meanings: Array.isArray(entry.meanings) ? entry.meanings : [entry.meanings],
     examples: Array.isArray(entry.examples) ? entry.examples : [entry.examples],
   };
+
+  const router = useTypedRouter();
+
+  const handleBack = () => router.back();
 
   const renderContent = () => {
     if (language === 'ja') {
@@ -24,7 +35,25 @@ function ExploreDictionaryEntryDetailView() {
     return <div>지원하지 않는 언어입니다.</div>;
   };
 
-  return <>{renderContent()}</>;
+  return (
+    <SlideInContainer>
+      <Box
+        sx={{
+          padding: `${spacing(2)} ${spacing(1)}`,
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          zIndex: Z_INDEX.backward,
+        }}
+      >
+        <IconButton onClick={handleBack} sx={{ padding: spacing(1) }}>
+          <ArrowBack sx={{ color: palette.text.primary }} />
+        </IconButton>
+      </Box>
+
+      <PageViewContainer>{renderContent()}</PageViewContainer>
+    </SlideInContainer>
+  );
 }
 
 export default ExploreDictionaryEntryDetailView;
